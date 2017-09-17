5fcf3fbd4f7bd6a5279d5cadaea1fd4a978c8355
Harun Djulic <harun@claireit.nl>
2017-09-17 20:36:52 +0200

    Tweaks on event detail modal

--- src/components/EventDetail/index.js
+++ src/components/EventDetail/index.js
@@ -10,13 +10,20 @@ const EventDetail = props => {
             <Grid columns={2} stackable className="EventDetail_grid">
                 <Grid.Column width={4} className="EventGeneralInfo">
                     <h1>We Are Developers</h1>
-                    <p className="detail">Vienna, Austria</p>
-                    <p className="detail">TestArena, Someplatz 21, Hauptstrasse Gasse</p>
-                    <p className="detail">11.05.2017 - 14.05.2017</p>
+                    <div className="-details">
+                        <p className="-detail">Vienna, Austria</p>
+                        <p className="-detail">TestArena, Someplatz 21, Hauptstrasse Gasse</p>
+                        <p className="-detail">11.05.2017 - 14.05.2017</p>
+                    </div>
 
                     <EventMap></EventMap>
                 </Grid.Column>
-                <Grid.Column width={12} className="EventDescription"></Grid.Column>
+                <Grid.Column width={12} className="EventDescription">
+                    <div className="-description">
+                        <h2>About this event</h2>
+                        <p>What is WeAreDevelopers all about? We are united tech professionals, empowered through code and as diverse as the languages we yield. In one of the leading startup capitals of Europe, the WeAreDevelopers World Congress brings together over 8,000 brightest minds in tech for an exciting event.</p>
+                    </div>
+                </Grid.Column>
             </Grid>
 
             <div className="EventDetailCloser" onClick={props.onClose}>
--- src/components/EventMap/index.js
+++ src/components/EventMap/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const EventMap = () => {
     return (
-        <div>
+        <div className="EventMap">
             MAPA
         </div>
     );
